0522a799543e7c382850235346c58950016df895
jesududu <jesududub@gmail.com>
2025-09-22 15:57:36 +0200

    Initial Commit

---
+++ PetGroomingApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PetGroomingApp
+//
+//  Created by Jesus Bueno on 22/9/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
